061f1836380909408029e544c01e24909e34e74f
Felipe de Sousa Nogueira <felipe.noug@gmail.com>
2024-02-04 20:58:24 -0300

    running1 in orchestrator

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ PATH_JSON = 'pipeline.json'
 
 if __name__ == '__main__':
 
-    data = file.get_json_from_file(file_path=PATH_JSON)
-
-    # for item in data:
-    #     print(f"{item}: {data[item]}")
-
-    print(config.get_pipeline_nodes(pipe_config_data=data))
+    raw_config_data = file.get_json_from_file(file_path=PATH_JSON)
+    prep_config_data = config.get_pipeline_nodes(pipe_config_data=raw_config_data)
+    pipe_class_list = config.classes_instantiator(list_pipe_config=prep_config_data)
