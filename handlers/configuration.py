e80116123666d1cbee27fec6296a0f18236b9e19
Felipe de Sousa Nogueira <felipe.noug@gmail.com>
2024-03-10 01:21:12 -0300

    refactor config script
    loggin add, config to configuration

---
+++ handlers/configuration.py
@@ -0,0 +1,44 @@
+import logging
+import os
+from logging import config
+
+import main
+
+MAIN_PATH = os.path.dirname(main.__file__)
+config.fileConfig(os.path.join(MAIN_PATH, 'logging.conf'))
+logger = logging.getLogger(__name__)
+
+
+def get_pipeline_nodes(pipe_config_data: dict) -> list:
+    """
+    Gets pipeline nodes from configure data and return as a dict list sorted by a "flow" string.
+
+        flow : "node_1->node_2->end" ==> [node_1,node_2]
+
+    :param pipe_config_data: dict with pipeline configuration data extracted from pipeline.json.
+
+    :return: list with dict nodes sorted by "flow" string in config file.
+    """
+    nodes_list = []
+    if pipe_config_data is None:
+        e = Exception("Pipeline config is invalid: dict is None.")
+        logger.error(e)
+        raise e
+    if len(pipe_config_data) < 2:
+        e = Exception("Pipeline config is invalid: config must have at last 1 node and flow string.")
+        logger.error(e)
+        raise e
+    try:
+        flow_str = pipe_config_data["flow"]
+        nodes_order = flow_str.split('->')
+
+        for node in nodes_order:
+            if node not in pipe_config_data.keys():
+                raise Exception(f"node '{node}' in flow string not exist in Pipeline config.")
+            nodes_list.append({node: pipe_config_data[node]})
+            logger.info(f"Node '{node}' add to queue list.")
+    except KeyError as e:
+        logger.error(f"Pipeline config is invalid: {e} not found in dict.")
+        raise e
+
+    return nodes_list
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from handlers import file, config
+import logging
+import os
+from logging import config
+from handlers import file, configuration
 from orchestrator import flow
+
+MAIN_PATH = os.path.dirname(__file__)
+config.fileConfig(os.path.join(MAIN_PATH, 'logging.conf'), disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 PATH_JSON = 'pipeline.json'
 
 if __name__ == '__main__':
 
     raw_config_data = file.get_json_from_file(file_path=PATH_JSON)
-    prep_config_data = config.get_pipeline_nodes(pipe_config_data=raw_config_data)
+    prep_config_data = configuration.get_pipeline_nodes(pipe_config_data=raw_config_data)
     data_cache = flow.execute(list_pipe_config=prep_config_data)
 
-    #print(prep_config_data)
     print(data_cache)
