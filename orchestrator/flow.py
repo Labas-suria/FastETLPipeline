5c14f5c339605402f64c47e0c36cf4ffdcca5202
Felipe de Sousa Nogueira <felipe.noug@gmail.com>
2024-03-10 01:57:55 -0300

    loggin add to flow method

--- orchestrator/flow.py
+++ orchestrator/flow.py
@@ -35,40 +35,51 @@ def execute(list_pipe_config: list):
         return data
 
     data_cache = []
-    for node in list_pipe_config:
-        node_name = list(node)[0]
-        node_class = node[node_name]['class']
-        node_type = node[node_name]['type']
-        node_params = node[node_name]['params']
-        match node_class:
-            case 'extract':
-                match node_type:
-                    case 'txt':
-                        data_cache.append({"extracted": TXT(**node_params).extract()})
-                    case _:
-                        raise Exception(f"The node type '{node_type}' is not supported in extract class.")
+    logger.info(f"Starting Pipeline...")
+    try:
+        for node in list_pipe_config:
+            node_name = list(node)[0]
+            node_class = node[node_name]['class']
+            node_type = node[node_name]['type']
+            node_params = node[node_name]['params']
 
-            case 'transform':
-                tmp_extr_data = get_first_data_in_cached_data(data_type="extracted")
-                if tmp_extr_data is None:
-                    raise Exception("No extracted data found to transform!")
+            logger.info(f"Starting '{node_name}' step...")
+            match node_class:
+                case 'extract':
+                    match node_type:
+                        case 'txt':
+                            data_cache.append({"extracted": TXT(**node_params).extract()})
+                        case _:
+                            raise Exception(f"The node type '{node_type}' is not supported in extract class.")
 
-                match node_type:
-                    case 'default':
-                        data_cache.append({"transformed": Transform(data=tmp_extr_data, **node_params).apply()})
-                    case _:
-                        raise Exception(f"The node type '{node_type}' is not supported in extract class.")
+                case 'transform':
+                    tmp_extr_data = get_first_data_in_cached_data(data_type="extracted")
+                    if tmp_extr_data is None:
+                        raise Exception("No extracted data found to transform!")
 
-            case 'load':
-                tmp_extr_data = get_first_data_in_cached_data(data_type="transformed")
-                if tmp_extr_data is None:
-                    raise Exception("No extracted data found to transform!")
+                    match node_type:
+                        case 'default':
+                            data_cache.append({"transformed": Transform(data=tmp_extr_data, **node_params).apply()})
+                        case _:
+                            raise Exception(f"The node type '{node_type}' is not supported in extract class.")
 
-                match node_type:
-                    case 'csv':
-                        data_cache.append({"loaded": CSV(data=tmp_extr_data, **node_params).load()})
+                case 'load':
+                    tmp_extr_data = get_first_data_in_cached_data(data_type="transformed")
+                    if tmp_extr_data is None:
+                        raise Exception("No extracted data found to transform!")
 
-            case _:
-                raise Exception(f"The node class '{node_class}' is not supported in instantiator.")
+                    match node_type:
+                        case 'csv':
+                            data_cache.append({"loaded": CSV(data=tmp_extr_data, **node_params).load()})
 
+                case _:
+                    raise Exception(f"The node class '{node_class}' is not supported in instantiator.")
+
+            logger.info(f"The '{node_name}' step is done!")
+
+    except Exception as e:
+        logger.error(e)
+        raise
+
+    logger.info("Pipeline Fineshed!")
     return data_cache
