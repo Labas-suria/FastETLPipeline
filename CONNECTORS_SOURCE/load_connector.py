7572a97c2775c86fbb991564c00bc977b19a9da3
Felipe de Sousa Nogueira <felipe.noug@gmail.com>
2024-06-02 18:38:20 -0300

    clean project to prod version

--- CONNECTORS_SOURCE/load_connector.py
+++
@@ -1,9 +0,0 @@
-from abstract_connectors.interfaces import AbstractLoad
-
-
-class LoadClass(AbstractLoad):
-    def __init__(self, data: list, **kwargs):
-        super().__init__(data=data, **kwargs)
-
-    def load(self):
-        return "false/path.txt"
--- CONNECTORS_SOURCE/transf_conector.py
+++
@@ -1,9 +0,0 @@
-from abstract_connectors.interfaces import AbstractTransform
-
-
-class TransformClass(AbstractTransform):
-    def __init__(self, data: list, **kwargs):
-        super().__init__(data=data, **kwargs)
-
-    def apply(self) -> list:
-        return [self.data]
