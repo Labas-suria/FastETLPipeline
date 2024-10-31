7df61c9e76090e7a3ede63de9856e0ba2c63561c
Felipe de Sousa Nogueira <felipe.noug@gmail.com>
2024-10-30 21:02:19 -0300

    flow class to dataframe

--- orchestrator/flow.py
+++ orchestrator/flow.py
@@ -3,6 +3,7 @@ import os
 from logging import config
 from queue import Queue
 
+from pandas import DataFrame
 from classes.txt import TXT
 from classes.transform import Transform
 from classes.csv import CSV
@@ -24,12 +25,13 @@ def __conector_caller(node_params: dict):
         script_import = node_params['script_import']
         class_name = node_params['class_name']
         script_import_list = script_import.split(".")
-        script_name = script_import_list[len(script_import_list) - 1]
+        script_name = script_import_list[-1]
 
         imp = __import__(script_import)
         imp_class = getattr(getattr(imp, script_name), class_name)
-        if type(imp_class(data=None, **node_params)).__base__ not in [AbstractTransform, AbstractExtract, AbstractLoad]:
-            raise Exception("The Conector must extends: AbstractTransform, AbstractExtract or AbstractLoad classes")
+        if not issubclass(type(imp_class(data=DataFrame(), **node_params)),
+                          (AbstractTransform, AbstractExtract, AbstractLoad)):
+            raise Exception("The Connector must extend: AbstractTransform, AbstractExtract, or AbstractLoad.")
 
     except Exception as e:
         logger.error(e)
@@ -40,36 +42,28 @@ def __conector_caller(node_params: dict):
 
 def execute(list_pipe_config: list, node_steps_list: Queue = None):
     """
-    Gets list with pipiline nodes in order, and execute each step of pipeline.
+    Executes the pipeline by processing each node step-by-step.
 
-    :param node_steps_list: list with fineshed nodes in flow, to show in UI.
-    :param list_pipe_config: list with dict nodes sorted by "flow" string in config file.
+    :param node_steps_list: Queue for tracking finished nodes in the pipeline.
+    :param list_pipe_config: List of nodes in order, defined by flow in the configuration.
     """
 
-    def get_first_data_in_cached_data(data_type: str) -> list:
-        """
-        Gets first data stored in data_cache for provided data_type, and remove it from cache.
-
-        :param data_type: "extracted" or "transformed"
-        :return: a data catched.
-        """
-        data = None
-
-        for index in range(0, len(data_cache)):
-            if data_type in data_cache[index].keys():
-                data = data_cache.pop(index)[data_type]
-                break
-
-        return data
+    def get_first_data_in_cached_data(data_type: str) -> DataFrame:
+        """Retrieves and removes the first DataFrame of the specified type from the cache."""
+        for index, cache_item in enumerate(data_cache):
+            if data_type in cache_item:
+                return data_cache.pop(index)[data_type]
+        return DataFrame()
 
     data_cache = []
     logger.info(f"Starting Pipeline...")
     try:
         for node in list_pipe_config:
             node_name = list(node)[0]
-            node_class = node[node_name]['class']
-            node_type = node[node_name]['type']
-            node_params = node[node_name]['params']
+            node_info = node[node_name]
+            node_class = node_info['class']
+            node_type = node_info['type']
+            node_params = node_info['params']
 
             logger.info(f"Starting '{node_name}' step...")
             match node_class:
@@ -79,101 +73,88 @@ def execute(list_pipe_config: list, node_steps_list: Queue = None):
                             data_cache.append({"extracted": TXT(**node_params).extract()})
                         case 'csv':
                             data_cache.append({"extracted": CSV(**node_params).extract()})
-
                         case 'connector':
-                            if 'script_import' not in node_params.keys() or 'class_name' not in node_params.keys():
-                                raise Exception("The 'script_import' and 'class_name' params must be sourced!")
-                            imp_class = __conector_caller(node_params=node_params)
-                            extrct_data = imp_class(**node_params).extract()
-
-                            if type(extrct_data) is not list:
-                                raise Exception("The connector returned data must be a list!")
-
-                            data_cache.append({"extracted": extrct_data})
-
+                            imp_class = __conector_caller(node_params)
+                            extracted_data = imp_class(**node_params).extract()
+                            if not isinstance(extracted_data, DataFrame):
+                                raise TypeError("The connector must return a DataFrame.")
+                            data_cache.append({"extracted": extracted_data})
                         case 'g_sheets':
                             creds = auth.run()
-                            data_cache.append({"extracted": Sheets(creds=creds, **node_params).extract(**node_params)})
-
+                            data_cache.append({"extracted": Sheets(creds, **node_params).extract(**node_params)})
                         case 'mysql':
                             data_cache.append({"extracted": MySQL().extract(**node_params)})
-
                         case 'postgresql':
                             data_cache.append({"extracted": PostgreSQL().extract(**node_params)})
-
                         case _:
-                            raise Exception(f"The node type '{node_type}' is not supported in extract class.")
+                            raise ValueError(f"Unsupported node type '{node_type}' for extraction.")
+
                 case 'transform':
-                    tmp_extr_data = get_first_data_in_cached_data(data_type="extracted")
-                    if tmp_extr_data is None:
-                        raise Exception("No extracted data found to transform!")
+                    extracted_data = get_first_data_in_cached_data("extracted")
+                    if extracted_data.empty:
+                        raise ValueError("No extracted data available for transformation.")
 
                     match node_type:
                         case 'default':
-                            data_cache.append({"transformed": Transform(data=tmp_extr_data, **node_params).apply()})
+                            transformed_data = Transform(extracted_data, **node_params).apply()
+                            data_cache.append({"transformed": transformed_data})
                         case 'connector':
-                            if 'script_import' not in node_params.keys() or 'class_name' not in node_params.keys():
-                                raise Exception("The 'script_import' and 'class_name' params must be sourced!")
-                            imp_class = __conector_caller(node_params=node_params)
-                            transf_data = imp_class(data=tmp_extr_data, **node_params).apply()
-
-                            if type(transf_data) is not list:
-                                raise Exception("The connector returned data must be a list!")
-
-                            data_cache.append({"transformed": transf_data})
-
+                            imp_class = __conector_caller(node_params)
+                            transformed_data = imp_class(extracted_data, **node_params).apply()
+                            if not isinstance(transformed_data, DataFrame):
+                                raise TypeError("The connector must return a DataFrame.")
+                            data_cache.append({"transformed": transformed_data})
                         case 'void':
-                            data_cache.append({"transformed": tmp_extr_data})
-
+                            data_cache.append({"transformed": extracted_data})
                         case _:
-                            raise Exception(f"The node type '{node_type}' is not supported in transform class.")
+                            raise ValueError(f"Unsupported node type '{node_type}' for transformation.")
 
                 case 'load':
-                    tmp_extr_data = get_first_data_in_cached_data(data_type="transformed")
-                    if tmp_extr_data is None:
-                        raise Exception("No transformed data found to load!")
+                    transformed_data = get_first_data_in_cached_data("transformed")
+                    if transformed_data.empty:
+                        raise ValueError("No transformed data available for loading.")
 
                     match node_type:
                         case 'csv':
-                            data_cache.append({"loaded": CSV(data=tmp_extr_data, **node_params).load()})
-
+                            CSV(data=transformed_data, **node_params).load()
+                            data_cache.append({"loaded": transformed_data})
                         case 'connector':
-                            if 'script_import' not in node_params.keys() or 'class_name' not in node_params.keys():
-                                raise Exception("The 'script_import' and 'class_name' params must be sourced!")
-                            imp_class = __conector_caller(node_params=node_params)
-                            transf_data = imp_class(data=tmp_extr_data, **node_params).load()
-
-                            data_cache.append({"loaded": transf_data})
-
+                            imp_class = __conector_caller(node_params)
+                            imp_class(transformed_data, **node_params).load()
+                            data_cache.append({"loaded": transformed_data})
                         case 'g_sheets':
                             creds = auth.run()
-                            data_cache.append({"loaded": Sheets(creds=creds, **node_params).load(data=tmp_extr_data,
-                                                                                                 **node_params)})
+                            Sheets(creds, **node_params).load(data=transformed_data, **node_params)
+                            data_cache.append({"loaded": transformed_data})
                         case 'mysql':
-                            data_cache.append({"loaded": MySQL().load(data=tmp_extr_data, **node_params)})
-
+                            MySQL().load(data=transformed_data, **node_params)
+                            data_cache.append({"loaded": transformed_data})
                         case 'postgresql':
-                            data_cache.append({"loaded": PostgreSQL().load(data=tmp_extr_data, **node_params)})
+                            PostgreSQL().load(data=transformed_data, **node_params)
+                            data_cache.append({"loaded": transformed_data})
+                        case _:
+                            raise ValueError(f"Unsupported node type '{node_type}' for loading.")
 
                 case _:
-                    raise Exception(f"The node class '{node_class}' is not supported in instantiator.")
+                    raise ValueError(f"Unsupported node class '{node_class}'.")
 
-            logger.info(f"The '{node_name}' step is done!")
-            if node_steps_list is not None:
+            logger.info(f"The '{node_name}' step is complete!")
+            if node_steps_list:
                 node_steps_list.put(node_name)
 
     except KeyError as e:
-        logger.error(f"Key Error in pipeline_config file: missing {e} attribute in '{node_name}' node.")
-        if node_steps_list is not None:
+        logger.error(f"KeyError in pipeline configuration: missing {e} in '{node_name}' node.")
+        if node_steps_list:
             node_steps_list.put('end')
         raise
     except Exception as e:
-        logger.error(f"Invalid pipeline file sintax, check app documentation! Error: {e}")
-        if node_steps_list is not None:
+        logger.error(f"Pipeline execution error: {e}")
+        if node_steps_list:
             node_steps_list.put('end')
         raise
 
-    logger.info("Pipeline Fineshed!")
-    if node_steps_list is not None:
+    logger.info("Pipeline execution finished!")
+    if node_steps_list:
         node_steps_list.put('end')
+
     return data_cache
